51195ffb17670f4414528cfca543fa73053174b1
zrotrasukha <zrotrasukha@gmail.com>
2026-01-06 00:49:22 +0530

    Add: Navbar and Hero Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import gsap from "gsap"
 import { ScrollTrigger, SplitText } from "gsap/all"
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 const App = () => {
     return (
-        <div className="flex-center h-screen">
-            <h1 className="text-3xl text-indigo-300">Hello, GSAP!</h1>
-        </div>
+        <>
+            <Navbar />
+            <Hero />
+        </>
     )
 }
 
