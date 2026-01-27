3b426165c67f55a38000c1751ad48654d6cb48ce
zrotrasukha <zrotrasukha@gmail.com>
2026-01-28 00:04:48 +0530

    Implementing: Art section

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
 import About from "./components/About";
+import Art from "./components/Art";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
             <Hero />
             <Cocktails />
             <About/>
+            <Art/>
         </main>
     )
 }
